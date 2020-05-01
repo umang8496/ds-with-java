57768c170be5f5bb4920ff250c272b3a2195a8d3
umang8496 <umang8april@gmail.com>
2020-05-01 12:17:11 +0530

    Find one dimensional peak.

--- ds-algo/src/ds/misc/OneDimensionalPeakFinder.java
+++ ds-algo/src/ds/misc/OneDimensionalPeakFinder.java
@@ -1,5 +1,46 @@
 package ds.misc;
 
-public class OneDimensionalPeakFinder {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+// example(1)
+// arr1=[61,62,24,28,32,14,36,61,6,12]
+// peaks=[62,32,61,12]
+// numberofpeaks=4
+
+// example(2)
+// arr2=[10,20,15,2,23,90,67]
+// peaks=[20,90]
+// numberofpeaks=2
+
+public class OneDimensionalPeakFinder {
+	public static void main(String[] args) {
+		int[] array = new int[] { 61, 62, 24, 28, 32, 14, 36, 61, 6, 12 };
+		oneDimensionalPeakFinder(array);
+	}
+
+	public static void oneDimensionalPeakFinder(int[] array) {
+		if (array != null) {
+			System.out.println("Given array is: " + Arrays.toString(array));
+			int lengthOfArray = array.length;
+			ArrayList<Integer> peaks = new ArrayList<>();
+
+			if (array[0] > array[1]) {
+				peaks.add(array[0]);
+			}
+
+			for (int f = 1; f < lengthOfArray - 1; f++) {
+				if (array[f] > array[f + 1] && array[f] > array[f - 1]) {
+					peaks.add(array[f]);
+				}
+			}
+
+			if (array[lengthOfArray - 1] > array[lengthOfArray - 2]) {
+				peaks.add(array[lengthOfArray - 1]);
+			}
+
+			System.out.println("Peaks in the given array:" + Arrays.toString(peaks.toArray()));
+			System.out.println("#peaks in the given array:" + peaks.size());
+		}
+	}
 }
