6554c351c257ec9442d8eeed4cf1cb5b2f5954d9
umang8496 <umang8april@gmail.com>
2020-05-17 01:00:07 +0530

    (Work In Progress): AvlTree has some bug here.

--- ds-algo/src/ds/tree/avl/AvlDemo.java
+++ ds-algo/src/ds/tree/avl/AvlDemo.java
@@ -3,11 +3,33 @@ package ds.tree.avl;
 public class AvlDemo {
 	public static void main(String[] args) {
 		AvlTree avl = new AvlTree();
-		avl.insertNode(20);
 		avl.insertNode(10);
+		avl.insertNode(20);
 		avl.insertNode(30);
-		avl.displayInOrder();
-		avl.displayPreOrder();
-		avl.displayPostOrder();
+		
+		avl.displayInOrder();		// InOrder : [ 10 20 30 ]
+		avl.displayPreOrder();		// PreOrder : [ 20 10 30 ]
+		avl.displayPostOrder();		// PostOrder : [ 10 30 20 ]
+		avl.displayLevelOrder();
+		
+		System.out.println();
+		System.out.println();
+		
+		avl.insertNode(40);
+		avl.insertNode(50);
+		
+		avl.displayInOrder();		// InOrder : [ 10 20 30 40 50 ]
+		avl.displayPreOrder();		// PreOrder : [ 20 10 40 30 50 ]
+		avl.displayPostOrder();		// PostOrder : [ 10 30 50 40 20 ]
+		avl.displayLevelOrder();
+		
+//		avl.insertNode(60);
+//		avl.insertNode(70);
+//		
+//		avl.displayInOrder();		// InOrder : [ 10 20 30 40 50 ]
+//		avl.displayPreOrder();		// PreOrder : [ 20 10 40 30 50 ]
+//		avl.displayPostOrder();		// PostOrder : [ 10 30 50 40 20 ]
+//		avl.displayLevelOrder();
 	}
+	
 }
