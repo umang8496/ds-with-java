e0d6ddef35cc7e670db635255bed2671d4cdfd39
umang8496 <umang8april@gmail.com>
2020-05-16 12:49:24 +0530

    Avl node template.

--- ds-algo/src/ds/tree/avl/AvlNode.java
+++ ds-algo/src/ds/tree/avl/AvlNode.java
@@ -1,5 +1,63 @@
 package ds.tree.avl;
 
 public class AvlNode {
+	private Integer data;
+	private AvlNode parent;
+	private AvlNode leftNode;
+	private AvlNode rightNode;
+	private int height;
+
+	private AvlNode() {
+		super();
+	}
+
+	public AvlNode(int data) {
+		this();
+		this.parent = null;
+		this.leftNode = null;
+		this.rightNode = null;
+		this.data = data;
+		this.height = 0;
+	}
+
+	public Integer getData() {
+		return data;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+
+	public AvlNode getParent() {
+		return parent;
+	}
+
+	public void setParent(AvlNode parent) {
+		this.parent = parent;
+	}
+
+	public AvlNode getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(AvlNode leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	public AvlNode getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(AvlNode rightNode) {
+		this.rightNode = rightNode;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 
 }
