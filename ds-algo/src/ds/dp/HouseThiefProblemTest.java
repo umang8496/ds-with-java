e6e6687e5c1c323f535225924314600129962571
umang8496 <umang8april@gmail.com>
2020-05-21 11:43:11 +0530

    House-Thief-Problem with tests.

---
+++ ds-algo/src/ds/dp/HouseThiefProblemTest.java
@@ -0,0 +1,23 @@
+package ds.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HouseThiefProblemTest {
+	@Test
+	public void test_1() {
+		Assert.assertEquals(4, HouseThiefProblem.rob(new int[] { 1, 2, 3, 1 }));
+	}
+
+	@Test
+	public void test_2() {
+		Assert.assertEquals(12, HouseThiefProblem.rob(new int[] { 2, 7, 9, 3, 1 }));
+	}
+
+	@Test
+	public void test_3() {
+		Assert.assertEquals(0, HouseThiefProblem.rob(new int[] { }));
+		Assert.assertEquals(-1, HouseThiefProblem.rob(null));
+	}
+	
+}
