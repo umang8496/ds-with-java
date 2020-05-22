9770547a9c448bcdee17bbffba6538992ed3ac40
umang8496 <umang8april@gmail.com>
2020-05-23 01:19:07 +0530

    Rod cutting problem through divide and conquer approach.

---
+++ ds-algo/src/ds/dp/RodCuttingProblemTest.java
@@ -0,0 +1,20 @@
+package ds.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RodCuttingProblemTest {
+
+	@Test
+	public void test_1() {
+		Assert.assertEquals(22, RodCuttingProblem.maximizeValueForRod(new int[] { 1, 5, 8, 9, 10, 17, 17, 20 }, 8));
+		Assert.assertEquals(10, RodCuttingProblem.maximizeValueForRod(new int[] { 1, 5, 8, 9 }, 4));
+		Assert.assertEquals(13, RodCuttingProblem.maximizeValueForRod(new int[] { 1, 5, 8, 9, 10 }, 5));
+	}
+
+	@Test
+	public void test_2() {
+
+	}
+
+}
