e49f516b952f474206fc2550ad50679d89c0b20b
umang8496 <umang8april@gmail.com>
2020-05-25 16:52:56 +0530

    MinHeap array based implementation.

--- ds-algo/src/ds/heap/HeapDemo.java
+++ ds-algo/src/ds/heap/HeapDemo.java
@@ -18,5 +18,23 @@ public class HeapDemo {
 		maxHeap.print();
 		System.out.println("The max val is " + maxHeap.remove());
 		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("The Min Heap is ");
+        MinHeap minHeap = new MinHeap(15);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+        minHeap.minHeap();
+ 
+        minHeap.print();
+        System.out.println("The Min val is " + minHeap.remove());
 	}
 }
